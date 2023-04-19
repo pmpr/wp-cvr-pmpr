87fc676b631635b56c3f9331be2a0f38b9027c08
pmpr.dev <pmpr.dev@gmail.com>
2023-04-19 14:01:52 +0000

    Obfuscated

--- src/Pmpr/Cover/Pmpr/Page/Common.php
+++ src/Pmpr/Cover/Pmpr/Page/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             64326dbeddf63             |
+    |             643ff41810269             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Common\Foundation\Page\Page; abstract class Common extends Page { }
--- src/Pmpr/Cover/Pmpr/Template/Template.php
+++ src/Pmpr/Cover/Pmpr/Template/Template.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             64326dbeddf63             |
+    |             643ff41810269             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr\Template; class Template extends Common { public function __construct() { parent::__construct(); $this->iemaakgqgqosiecm(); } public function iemaakgqgqosiecm() { Card::symcgieuakksimmu(); Relation::symcgieuakksimmu(); } }
