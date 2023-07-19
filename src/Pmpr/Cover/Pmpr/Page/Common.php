cbf6624136c27aa7ba65f8447f44359df27e7eaa
pmpr.dev <pmpr.dev@gmail.com>
2023-07-19 06:55:41 +0000

    Obfuscated

--- src/Pmpr/Cover/Pmpr/Page/Common.php
+++ src/Pmpr/Cover/Pmpr/Page/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             64aaa0ee77b98             |
+    |             64b788bb26a99             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr\Page; use Pmpr\Common\Foundation\Page\Page; abstract class Common extends Page { }
--- src/Pmpr/Cover/Pmpr/Template/Common.php
+++ src/Pmpr/Cover/Pmpr/Template/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             64aaa0ee77b98             |
+    |             64b788bb26a99             |
     |_______________________________________|
 */
  namespace Pmpr\Cover\Pmpr\Template; use Pmpr\Cover\Pmpr\Container; class Common extends Container { }
